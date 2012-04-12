8ede512dab6ce3862fc8486736220fea9bce34b4
Steinn <ses@s.imcisland.is>
2012-04-12 04:01:28 +0000

    db loading almost ready.. needs some testing/verifying

--- dbload.py
+++ dbload.py
@@ -7,20 +7,39 @@ import sqlite3
 default_datadir = "./data"
 
 def insert_user(cursor, rec):
-	return cursor.execute("INSERT INTO user (id,age,gender,occupation,zipcode) VALUES (?, ?, ?, ?, ?)", rec)
+	return cursor.execute("INSERT INTO user (id,age,gender,occupation,zipcode) VALUES (?, ?, ?, ?, ?)", rec[:5])
 
 def insert_movie(cursor, rec):
-	return cursor.execute("INSERT INTO movie (id,name,releasedate,url) VALUES (?, ?, ?, ?)", rec)
+	return cursor.execute("INSERT INTO movie (id,name,releasedate,url) VALUES (?, ?, ?, ?)", rec[:4])
 
 def insert_genre(cursor, rec):
-	return cursor.execute("INSERT INTO genre (name) VALUES (?)", rec)
+	return cursor.execute("INSERT INTO genre (name) VALUES (?)", rec[:1])
 
 def insert_movie_genre(cursor, rec):
-	return cursor.execute("INSERT INTO movie_genre (id,movieid,genreid) VALUES (?, ?, ?)", rec)
+	return cursor.execute("INSERT INTO movie_genre (movieid,genreid) VALUES (?, ?)", rec[:2])
 
 def insert_user_rating(cursor, rec):
-	return cursor.execute("INSERT INTO user_ratings (id,uid,mid,rating,timestamp) VALUES (?,?,?,?,?)", rec)
+	return cursor.execute("INSERT INTO user_ratings (uid,mid,rating,timestamp) VALUES (?,?,?,?)", rec[:4])
 
+def loadGenres(cursor):
+	cursor.execute("SELECT * FROM genre")
+	gm = {}
+	for row in cursor.fetchall():
+		gm[row[0]] = row[1]
+	return gm
+
+def setGenres(cursor, rec):
+	mid = rec[0]
+	garr = rec[5:]
+	genre_map = loadGenres(cursor)
+	genres = ['unknown', 'Action' 'Adventure', 'Animation', 'Children\'s', 'Comedy', 'Crime' 'Documentary', 'Drama', 'Fantasy', 'Film-Noir', 'Horror', 'Musical', 'Mystery', 'Romance', 'Sci-Fi', 'Thriller', 'War', 'Western' ]
+	r = []
+	i = 0
+	for n in garr:
+		if (n == 1):
+			gid = genre_map[genres[i]]
+			insert_movie_genre(cursor, list([mid, gid]))
+		i += 1
 
 def main():
 	if (len(sys.argv) < 2):
@@ -47,6 +66,7 @@ def main():
 
 	print "Opening database: %s"%database
 	db = sqlite3.connect(database)
+	db.text_factory = str
 	c = db.cursor()
 	# special handling for genres!
 	genres = ['unknown', 'Action' 'Adventure', 'Animation', 'Children\'s', 'Comedy', 'Crime' 'Documentary', 'Drama', 'Fantasy', 'Film-Noir', 'Horror', 'Musical', 'Mystery', 'Romance', 'Sci-Fi', 'Thriller', 'War', 'Western' ]
@@ -55,7 +75,35 @@ def main():
 		if not insert_genre(c, list([g])):
 			print "Failed to insert: %s"%g
 
-	#db.commit()
+	data = [ { 'name': 'user',
+		   'insert':insert_user,
+	  	   'file':'u.user',
+		   'sep':'|'
+		 },
+		 { 'name': 'movie',
+		   'insert':insert_movie,
+	 	   'file':'u.item',
+		   'sep':'|',
+		   'post_proc':setGenres
+		 },
+		 { 'name': 'user_rating',
+		   'insert': insert_user_rating,
+		   'file':'u.data',
+		   'sep':"\t"
+                 }
+		]
+
+	for dsn in data:
+		print "Loading %s"%dsn['name']
+		for line in open(datadir + "/" + dsn['file']).readlines():
+			rec = line.strip().split(dsn['sep'])
+			if (not dsn['insert'](c, list(rec))):
+				print "failed to insert:"
+				print rec
+			if dsn.has_key('post_proc'):
+				dsn['post_proc'](c, list(rec))
+
+	db.commit()
 		
 if __name__ == '__main__':
 	main()
